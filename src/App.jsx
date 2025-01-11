83a06d6b4eb534dae3600b0e909697b9e372cef8
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2025-01-11 23:18:55 +0100

    project setUp

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import ErrorBoundary from './components/ErrorBoundary'
 import Hero from './components/Hero'
+import Test from './components/Test'
 
 function App() {
   return (
     <ErrorBoundary>
       <body className='style="overflow: hidden; height: 100%; width: 100%; visibility: visible; display: block; inset: 0px auto auto 0px; margin: 0px; opacity: 1 !important;'>
-      <Header/>
-      <Hero/>
+      {/* <Header/>
+      <Hero/> */}
+      <Test/>
       </body>
    
     </ErrorBoundary>
