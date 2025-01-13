7843fd28b44dd310f3aabb52a7b5b37debee15d9
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2025-01-13 23:09:34 +0100

    display

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <ErrorBoundary>
       <body className='style="overflow: hidden; height: 100%; width: 100%; visibility: visible; display: block; inset: 0px auto auto 0px; margin: 0px; opacity: 1 !important;'>
-      {/* <Header/>
-      <Hero/> */}
+      <Header/>
+      <Hero/>
       <Test/>
       </body>
    
